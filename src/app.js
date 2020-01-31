9079f328d79e5c217aa2fcce1e88e1e94afee4ad
杨国保 <yangguobao@edaijia-inc.cn>
2020-01-31 14:34:09 +0800

    实现根据请求路径判断是文件还是文件夹；如果是文件就返回内容，是文件夹就返回文件列表

--- src/app.js
+++ src/app.js
@@ -1,14 +1,35 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 const chalk = require('chalk');
 const {promisify} = require('util');
 
+const stat = promisify(fs.stat);
+const readDir = promisify(fs.readdir);
+
 const config = require('./config');
 
-const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
-    res.setHeader('Set-Cookie', ['type=ninja', 'language=javascript']);
-    res.end('<html><head><title>haha</title></head><body>nihaoa</body></html>');
+const app = http.createServer(async (req, res) => {
+    const {url} = req;
+    const filePath = path.join(config.root, url);
+    try {
+        const stats = await stat(filePath);
+        if (stats.isFile()) {
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'text/plain; charset=utf-8');
+            fs.createReadStream(filePath).pipe(res);
+        }
+        if (stats.isDirectory()) {
+            const files = await readDir(filePath);
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'text/plain; charset=utf-8');
+            res.end(files.join(','));
+        }
+    } catch (err) {
+        res.statusCode = 404;
+        res.setHeader('Content-type', 'text/plain; charset=utf-8');
+        res.end('没有找到请求的文件');
+    }
 });
 
 app.listen(config.port, config.host, () => {
