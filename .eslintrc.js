be4a2dfd2fa4390f3c5e67c329d8779e7c506b64
杨国保 <yangguobao@edaijia-inc.cn>
2020-01-31 12:03:11 +0800

    配置.eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "globals": {
+        "window": true
+    },
+    "parser": "esprima",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+    ],
+    "rules": {
+        "no-console": [
+            0
+        ]
+    }
+};
